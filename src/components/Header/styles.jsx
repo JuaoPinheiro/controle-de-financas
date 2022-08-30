31b9be46f4fb5a5f21022686259b4e83ba7cde56
JuaoPinheiro <juaopinheiro26@gmail.com>
2022-08-30 15:11:12 -0300

    finished

---
+++ src/components/Header/styles.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    height: 150px;
+    text-align: center;
+    background: rgb(76,53,84);
+    background: linear-gradient(0deg, rgba(76,53,84,1) 0%, rgba(181,0,255,1) 100%);
+`
+
+export const Header = styled.div``
+
+export const Title = styled.h1`
+    padding-top: 20px;
+    color: #fff;
+`
